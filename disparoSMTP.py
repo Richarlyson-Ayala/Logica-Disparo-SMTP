4172893ca7aeded41d120d9f6056fe4d4c7459de
Richarlyson Ayala de Oliveira Abreu <richarlyson.ayala@guaraves.local>
2025-08-08 09:04:51 -0300

    Reasjute Geral

--- disparoSMTP.py
+++ disparoSMTP.py
@@ -1,12 +1,23 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
-def ler_Arquivo(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        return file.read()
     
-def send_email(solicitacion, receiver_email, subject, link, html_content, type, manager, title):
+def montar_Html(html_content, messages, link, type):
+    for cont in range(len(messages[type])):
+       html_content = html_content.replace(f"{{msg{cont + 1}}}", messages[type][cont])
+    html_content = html_content.replace(
+        "{link}",
+        f'''<a href="{link}" 
+            style="background-color:#9d00a0c7;color:#f3f3f3;border:none;padding:1rem 1.4rem;
+            border-radius:0.3rem;cursor:pointer;font-size:1rem;box-shadow:0 2px 8px rgba(0,0,0,0.1);
+            margin:0 auto;text-decoration:none;display:inline-block;width:25%;transition:0.4s ease;"
+        >Acompanhar Solicitação</a>'''
+    )
+    return html_content
+    
+def send_email(manager, solicitacion, subject, title, receiver_email, link, html_content, type):
+
+    # Informações necessárias para o envio do e-mail
     sender_email = "ronyelison78@gmail.com"
     sender_password = "szkh qyun cnvj uotq" # Use uma senha de app para Gmail
     messages = [[f'Prezado(a) {manager},',
@@ -36,17 +47,8 @@ def send_email(solicitacion, receiver_email, subject, link, html_content, type, 
     msg["To"] = receiver_email
     msg["Subject"] = subject
 
-    for cont in range(len(messages[type])):
-       html_content = html_content.replace(f"{{msg{cont + 1}}}", messages[type][cont])
-       if cont == 4:
-            html_content = html_content.replace(
-                "{link}",
-                f'''<a href="{link}" 
-                    style="background-color:#9d00a0c7;color:#f3f3f3;border:none;padding:1rem 1.4rem;
-                    border-radius:0.3rem;cursor:pointer;font-size:1rem;box-shadow:0 2px 8px rgba(0,0,0,0.1);
-                    margin:0 auto;text-decoration:none;display:inline-block;widht:50%;transition:0.4s ease;"
-                >Acompanhar Solicitação</a>'''
-            )
+    # Trocar as mensagens {msg1}, {msg2}, ... e o {link} no conteúdo HTML
+    html_content = montar_Html(html_content, messages, link, type)
 
     # Anexar o conteúdo HTML
     msg.attach(MIMEText(html_content, "html"))
@@ -70,7 +72,139 @@ def send_email(solicitacion, receiver_email, subject, link, html_content, type, 
 
 if __name__ == "__main__":
     # Ler o arquivo HTML
-    html_content = ler_Arquivo("index.html")
+    html_content = '''<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Guaraves</title>
+        <style>
+            * {
+                box-sizing: border-box;
+                margin: 0;
+                padding: 0;
+            }
+            
+            body {
+                font-family: calibri, sans-serif;
+                margin: 0 auto;
+                background-color: #f4f4f4;
+                font-size: 1.125rem; 
+                letter-spacing: 0.5px;
+                width: 50%;
+            }
+
+            #colorbox {
+                background-color: #9d00a0c7;
+                color: white;
+                padding:  1.25rem; 
+                text-align: center;
+                border-top-right-radius: 1.25rem;
+                border-top-left-radius: 1.25rem;
+                margin-top: 1rem;
+            }
+
+            #content {
+                padding: 1.25rem;
+                
+            }
+
+            #colorbox p{
+                opacity: 0.5;
+            }
+            #colorbox h1 {
+                font-weight: 600;
+            }
+            #content h4{
+                background: #8c8c8c29;
+                border-left: 6px solid #9d00a0c7;
+                border-top-right-radius: 12px;
+                border-bottom-right-radius: 12px;
+                padding: 24px;
+                margin: 40px auto;
+                max-width: 1400px;
+                box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+                text-align: left;
+                font-weight: 400;
+                opacity: 0.75;
+            }
+            .purple {
+                color: #9d00a0c7;
+                text-decoration: none;
+            }
+
+            #content p {
+                line-height: 1.6;
+                font-size: 1.25rem;
+            }
+
+            #button-container {
+                text-align: center;
+                margin: 3rem 0;
+                width: 100%;
+            }
+
+            /*  Estilização que ta vindo do python
+            .color-button {
+                background-color: #9d00a0c7;
+                color: #f3f3f3;
+                border: none;
+                padding: 1rem 1.4rem;
+                border-radius: 0.3rem;
+                cursor: pointer;
+                font-size: 1rem;
+                box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+                margin: 0 auto;
+                transition: 0.4s ease;
+                text-decoration: none;
+                width: 50%;
+            } */
+
+            .color-button:hover {
+                background-color: #8c00a0c7;
+            }
+
+            #finally {
+                padding: 1.25rem;
+                font-size: 1.25rem;
+                line-height: 1.6;
+            }
+            </style>
+        </head>
+
+        <body>
+            <div id="colorbox">
+                <h1>GIT.flow</h1>
+                <p>Confirmação de Cadastro</p>
+            </div>
+            <div id="content">
+                <h4>Solicitação criada com sucesso!</h4>
+                <p>{msg1}
+                    <br><br>
+                    {msg2}
+                    <br><br>
+                    {msg3}
+                    <br><br>
+                    {msg4}
+                </p>
+            </div>
+            <div id="button-container">
+                {link}
+            </div>
+            <div id="finally">
+                <p>Atenciosamente,</p>
+                <span class="purple">Equipe GIT.flow</span>
+            </div>
+        </body>
+        </html>'''
 
     # Enviar o e-mail
-    send_email("Rony", "gabriel.nunes@guaraves.com.br", "Testando Email", "google.com", html_content, 2, "Rodrigo", "Checar páginas")
+    send_email("Wedson",
+                "Richarlyson",
+                "Aprovação de Documentação",
+                "Checar Documentação",  
+                "gabriel.nunes@guaraves.com.br",
+                "google.com",
+                html_content,
+                1
+                )
